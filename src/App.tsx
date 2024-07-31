8da2567307669d389eb09a819e933294e1d89788
VitorCostaTI <vitortecnologia62@gmail.com>
2024-07-31 01:44:10 -0300

    Configuração arquitetura de projeto

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text} from "react-native";
+
+export const App: React.FC = () => {
+    return(
+        <View>
+            <Text>
+                Hello World - Wallet App
+            </Text>
+        </View>
+    )
+}
