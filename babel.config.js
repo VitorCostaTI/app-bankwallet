176179c42119504dd2da6e38c7e8dc344179007e
VitorCostaTI <vitortecnologia62@gmail.com>
2024-07-31 02:07:03 -0300

    Instalação Babel-Plugin-Module

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,15 @@
-module.exports = function(api) {
+const { default: plugin } = require("babel-plugin-module-resolve");
+
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      ["module-resolver", {
+        alias: {
+          "@src": "./src/*",
+        }
+      }]
+    ]
   };
 };
